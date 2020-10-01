05887cc44579fe8c7bd95aac298db05b7cd640e0
byronSanty <bscabezas@uce.edu.ec>
2020-09-30 23:58:42 -0500

    objectCreated

--- inside.py
+++ inside.py
@@ -1,1 +1,21 @@
-print('Ingeniero Willian Fernando Vasquez mi preferido')
+print('Usuario Correspondiente')
+class Person:
+    pass
+jorge =  Person()
+
+class Person:
+    name = ''
+    school = ''
+    city = ''
+    year = ''
+    
+jorge = Person()
+jorge.name = 'Jorge'
+jorge.school = 'Mejia'
+jorge.city = 'Quito'
+jorge.year = '2020'
+
+print('Person - name >>',jorge.name)
+print('Person - school >>',jorge.school)
+print('Person - city >>',jorge.city)
+print('Person - year >>',jorge.year)
